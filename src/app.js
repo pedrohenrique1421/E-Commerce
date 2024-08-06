7fccbac94a3733e8a4cdfa02db5f5661e33e7ae5
Pedro <Phenriquelins1@gmail.com>
2024-08-06 16:29:05 -0300

    Alocando para o Firebase Firestore

--- src/app.js
+++ src/app.js
@@ -3,21 +3,11 @@ import FornecedorRouter from "./Routers/Fornecedores.js";
 
 const app = express();
 const port = 3030;
+
 app.use(express.json());
 
 app.use("/fornecedor", FornecedorRouter);
 
-app.post("/", function (req, res) {
-  res.send("Got a POST request");
-});
-
-app.put("/user", function (req, res) {
-  res.send("Got a PUT request at /user");
-});
-
-app.delete("/user", function (req, res) {
-  res.send("Got a DELETE request at /user");
-});
 
 app.listen(port, () => {
   console.log(`Escutando a porta: ${port}`);
